15c62bbe3d397913d1d273e62d8dbe1c6afbadce
Xilop-Tea <xilopgaming@gmail.com>
2026-03-02 16:11:58 +1100

    Finished Unicon

--- Unit Converter/main.go
+++ Unit Converter/main.go
@@ -24,37 +24,87 @@ func g2oz(f float64) {
 	fmt.Println(f * 0.03527396195)
 }
 
-func main() {
-	conType := os.Args[1]
-	conNum := os.Args[2]
-	conNumf, _ := strconv.ParseFloat(conNum, 64)
-	validType := false
+// Distance
+func mi2km(f float64) {
+	fmt.Println(f * 1.609344)
+}
 
-	fmt.Print("\n")
+func km2mi(f float64) {
+	fmt.Println(f * 0.6214)
+}
+
+func ft2m(f float64) {
+	fmt.Println(f * 0.3048)
+}
+
+func m2ft(f float64) {
+	fmt.Println(f * 3.28084)
+}
+
+func in2cm(f float64) {
+	fmt.Println(f * 2.54)
+}
+
+func cm2in(f float64) {
+	fmt.Println(f * 0.3937008)
+}
+
+func in2mm(f float64) {
+	fmt.Println(f * 25.4)
+}
+
+func mm2in(f float64) {
+	fmt.Println(f * 0.03937008)
+}
+
+func main() {
+	var helpText string = `Valid Converts:
+	lb2kg	kg2lb
+	oz2g	g2oz
+	mi2km	km2mi
+	ft2m	m2ft
+	in2cm	cm2in
+	in2mm	mm2in`
+	conType := os.Args[1]
+	var conNum string //Number is string beacuse inputs are strings, needs to be converted to float
+
+	if len(os.Args) > 2 {
+		conNum = os.Args[2]
+	} else {
+		conNum = "1"
+	}
+
+	conNumf, _ := strconv.ParseFloat(conNum, 64)
+
 	//Converters
 	if strings.Compare(conType, "lb2kg") == 0 {
 		lb2kg(conNumf)
-		validType = true
-	}
-
-	if strings.Compare(conType, "kg2lb") == 0 {
+	} else if strings.Compare(conType, "kg2lb") == 0 {
 		kg2lb(conNumf)
-		validType = true
-	}
-
-	if strings.Compare(conType, "oz2g") == 0 {
+	} else if strings.Compare(conType, "oz2g") == 0 {
 		oz2g(conNumf)
-		validType = true
-	}
-
-	if strings.Compare(conType, "g2oz") == 0 {
+	} else if strings.Compare(conType, "g2oz") == 0 {
 		g2oz(conNumf)
-		validType = true
+	} else if strings.Compare(conType, "mi2km") == 0 {
+		mi2km(conNumf)
+	} else if strings.Compare(conType, "km2mi") == 0 {
+		km2mi(conNumf)
+	} else if strings.Compare(conType, "ft2m") == 0 {
+		ft2m(conNumf)
+	} else if strings.Compare(conType, "m2ft") == 0 {
+		m2ft(conNumf)
+	} else if strings.Compare(conType, "in2cm") == 0 {
+		in2cm(conNumf)
+	} else if strings.Compare(conType, "cm2in") == 0 {
+		cm2in(conNumf)
+	} else if strings.Compare(conType, "in2mm") == 0 {
+		ft2m(conNumf)
+	} else if strings.Compare(conType, "mm2in") == 0 {
+		mm2in(conNumf)
+	} else if strings.Compare(conType, "help") == 0 {
+		fmt.Println(helpText)
+	} else {
+		fmt.Println("Invalid Convert. Type unicon help to see all valid converts")
 	}
-	if !validType {
-		fmt.Println("Invalid")
-	}
-
-	fmt.Print("\n")
 
 }
